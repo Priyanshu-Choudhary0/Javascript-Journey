b2a1ef061fc7b80ee21472f84480be54aad1862d
Priyanshu-Choudhary0 <priyanshuchoudhary883@gmail.com>
2025-01-05 17:08:57 +0000

    Stack and Heap Memory

---
+++ 04 - Javascript/index.js
@@ -0,0 +1,6 @@
+// Stack (Primitive), Heap (Non-Primitive)
+let myYoutubename = "priyanshuchoudharydotcom"
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+console.log(myYoutubename);
+console.log(anothername);
